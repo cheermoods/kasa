397d00b323df772c5d57d0ee2ed25a5b392c1e46
cheermoods <tojo.ranto@outlook.fr>
2025-01-30 10:42:55 +0100

    Lancement du projet

---
+++ src/component/header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="./images/logo.png" alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#home">Accueil</a></li>
+          <li><a href="#about">À propos</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
